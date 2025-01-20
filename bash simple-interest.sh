5df7beabf14e95ae5ea02bea954bce657e118058
Mohamed Rabhi <62715238+Mohamedrebhi@users.noreply.github.com>
2025-01-20 14:55:53 +0100

    Create bash simple-interest.sh

---
+++ bash simple-interest.sh
@@ -0,0 +1,29 @@
+
+## Contributing
+Please read [CONTRIBUTING.md](CONTRIBUTING.md) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## License
+This project is licensed under the Apache License 2.0 - see the [LICENSE](LICENSE) file for details
+# Contributing to Simple Interest Calculator
+
+We love your input! We want to make contributing to Simple Interest Calculator as easy and transparent as possible, whether it's:
+
+- Reporting a bug
+- Discussing the current state of the code
+- Submitting a fix
+- Proposing new features
+- Becoming a maintainer
+
+## We Develop with Github
+We use GitHub to host code, to track issues and feature requests, as well as accept pull requests.
+
+## Pull Requests
+1. Fork the repo and create your branch from `main`.
+2. If you've added code that should be tested, add tests.
+3. If you've changed APIs, update the documentation.
+4. Ensure the test suite passes.
+5. Make sure your code lints.
+6. Issue that pull request!
+
+## Any contributions you make will be under the Apache License 2.0
+When you submit code changes, your submissions are understood to be under the same [Apache License 2.0](LICENSE) that covers the project.
